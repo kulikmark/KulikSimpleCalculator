22d13cb35e7800e856bbc9d302b4302bfa20a322
Mark Kulik <kulik_mark@mail.ru>
2024-03-27 12:31:53 +0300

    Initial Commit

---
+++ KulikSimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KulikSimpleCalculator
+//
+//  Created by Марк Кулик on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
